6344e40cd383f8ebc242faccf272ac487dc53167
BONTHU ANITHA <121091961+Anithabonthu@users.noreply.github.com>
2024-04-07 17:20:01 +0530

    Add files via upload
    Second largest element in BST:
    Given a binary tree, convert the binary tree into its Mirror tree. Mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes interchanged.
    Inorder Traversal and 1st index is second largest element

---
+++ FindingSecondLargestElement.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Node:  
+    def __init__(self,value):  
+        self.left=None  
+        self.data=value  
+        self.right=None  
+l=[]
+class BinarySearchTree:  
+    def __init__(self):  
+        self.root=None  
+    def insert(self,value):  
+        newnode=Node(value)  
+        if self.root is None:  
+            self.root=newnode  
+        else:  
+            current=self.root  
+            while True:  
+                if value<current.data:  
+                    if current.left is None:  
+                        current.left=newnode  
+                        break  
+                    else:  
+                        current=current.left  
+                elif current.data<value:  
+                    if current.right is None:  
+                        current.right=newnode  
+                        break  
+                    else:  
+                        current=current.right  
+                else:  
+                    break  
+                    
+    def inorder(self,current):   
+        if current:   
+            self.inorder(current.left)   
+            l.append(current.data) 
+            self.inorder(current.right)   
+
+    def mirror_image(self,node): 
+        if node is None: 
+            return 
+
+        else: 
+            self.mirror_image(node.left) 
+            self.mirror_image(node.right) 
+            temp=node.left 
+            node.left=node.right 
+            node.right=temp 
+
+bt=BinarySearchTree()  
+while True:  
+    print("1.insert  2.Exit")  
+    ch=int(input("Enter your choice:"))  
+    if ch==1:  
+        value=int(input("Enter value:"))  
+        bt.insert(value)           
+    elif ch==2: 
+        break  
+
+bt.mirror_image(bt.root)
+bt.inorder(bt.root)           
+print(l)
+print("Second Largest Element is:",l[1])
+
+
+# In[ ]:
+
+
+
+
